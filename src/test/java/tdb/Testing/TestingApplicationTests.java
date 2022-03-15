af53de8cbe17a5dee1283fe416c1a4a882d550c9
Sainadh Gosukula <sainsam75@gmail.com>
2022-03-16 00:17:47 +0530

    Uploading Code

---
+++ src/test/java/tdb/Testing/TestingApplicationTests.java
@@ -0,0 +1,13 @@
+package tdb.Testing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
