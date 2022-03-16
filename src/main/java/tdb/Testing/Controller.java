2de1e5ac5c997b5ac2c016a65af89c5b5972c2b2
Sainadh Gosukula <sainsam75@gmail.com>
2022-03-16 09:25:03 +0530

    Working

--- src/main/java/tdb/Testing/Controller.java
+++ src/main/java/tdb/Testing/Controller.java
@@ -1,12 +1,15 @@
 package tdb.Testing;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Controller {
-    @GetMapping("/")
+    @Autowired
+    private Serviceclass serviceclass;
+    @GetMapping("/home")
     public String getHome(){
-        return "Hello Azure Web Services";
+        return serviceclass.message();
     }
 }
---
+++ src/main/java/tdb/Testing/Serviceclass.java
@@ -0,0 +1,11 @@
+package tdb.Testing;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class Serviceclass {
+
+    public String message(){
+        return " HELLO AZURE WEB APP SERVICE";
+    }
+}
